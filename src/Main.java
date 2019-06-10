8e852995227cc602b47009f3d1016c6a38fb9779
Rafael Xavier <rafael_ldsx@hotmail.com>
2019-06-10 10:18:03 -0300

    Troca de id do client

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) throws SocketException, UnknownHostException {
 		
-		Thread t1 = new Thread(new Client(1));
+		Thread t1 = new Thread(new Client(2));
 		
 		t1.start();
 
