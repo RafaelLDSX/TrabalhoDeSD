ec1a7f169c60707f14e692b9e96f1c162e280fce
Andrei Parent <andreiparente@gmail.com>
2019-07-04 05:12:44 -0300

    Funcionando com dois pelo menos

--- src/states/Election.java
+++ src/states/Election.java
@@ -23,10 +23,13 @@ public class Election implements State{
 	public String answer(String msg) {
 		if(msg.contains("Meu id e")) {
 			this.silenceCounter = 0;
-			int opennedBracketIndex = msg.indexOf("[");
-			int closedBracketIndex = msg.indexOf("]");
-			String toProcess = msg.substring(opennedBracketIndex+1, closedBracketIndex);
-			return comparison(Integer.parseInt(toProcess));
+			//int opennedBracketIndex = msg.indexOf("[");
+			//int closedBracketIndex = msg.indexOf("]");
+			//String toProcess = msg.substring(opennedBracketIndex+1, closedBracketIndex);
+			msg = msg.replace("Meu id e ", "");
+			msg = msg.replace("[", "");
+			msg = msg.replace("]", "");
+			return comparison(Integer.parseInt(msg.trim()));
 		}
 		else if(msg.equals("")) {
 			this.silenceCounter += 1;
